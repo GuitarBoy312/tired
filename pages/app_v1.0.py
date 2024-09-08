d5af2883892dbce2e94fc950de1cd30f5d89dfe6
GuitarBoy312 <169133828+GuitarBoy312@users.noreply.github.com>
2024-09-09 05:34:31 +0900

    Update app_v1.0.py

--- pages/app_v1.0.py
+++ pages/app_v1.0.py
@@ -6,27 +6,19 @@ from io import BytesIO
 import os
 from pydub import AudioSegment
 from pydub.playback import play
-from langdetect import detect
+
 
 # OpenAI API 키 설정 (OpenAI 계정에서 발급받은 API 키를 넣어주세요)
 client = OpenAI(
   api_key=st.secrets["openai_api_key"], 
 )
-# 음성 인식 및 변환
-recognizer = sr.Recognizer()
-
-def recognize_speech():
-    with sr.Microphone() as source:
-        st.info("음성 입력을 기다리고 있습니다...")
-        audio = recognizer.listen(source)
-        st.success("음성 입력이 완료되었습니다.")
-        try:
-            text = recognizer.recognize_google(audio, language='ko-KR')
-            return text
-        except sr.UnknownValueError:
-            return "음성을 인식할 수 없습니다."
-        except sr.RequestError as e:
-            return f"음성 인식 서비스에 문제가 발생했습니다: {e}"
+# 텍스트를 음성으로 변환하여 재생하는 함수
+def text_to_speech(text, lang='ko'):
+    tts = gTTS(text=text, lang=lang)
+    audio_file = BytesIO()
+    tts.write_to_fp(audio_file)
+    audio_file.seek(0)
+    
 
 # ChatGPT API 호출
 def get_chatgpt_response(prompt):
@@ -41,7 +33,7 @@ def get_chatgpt_response(prompt):
 
 # 음성을 텍스트로 변환하고, 텍스트를 다시 음성으로 변환
 def text_to_speech(text):
-    tts = gTTS(text=text, lang='en' or 'ko')
+    tts = gTTS(text=text, lang='en')
     audio_file = BytesIO()
     tts.write_to_fp(audio_file)
     audio_file.seek(0)
