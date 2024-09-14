4ca4096e48ef6588c768e5f55b26c9982706985d
GuitarBoy312 <169133828+GuitarBoy312@users.noreply.github.com>
2024-09-15 01:23:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import io
 client = OpenAI(api_key=st.secrets["openai_api_key"])
 
 # ChatGPT API 호출
-def get_chatgpt_response(prompt):
+def get_chatgpt_response(messages):
     response = client.chat.completions.create(
         model="gpt-4o-mini",  # 사용할 모델
         messages=[
@@ -39,7 +39,7 @@ I'm Happy
 - tired
              '''
              },
-            {"role": "user", "content": prompt}
+            {"role": "user", "content": messages}
         ]
     )
     return response.choices[0].message.content
