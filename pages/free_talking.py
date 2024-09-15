0a896e41aa34a41a282f592426fa644cc4492b44
GuitarBoy312 <169133828+GuitarBoy312@users.noreply.github.com>
2024-09-15 17:01:12 +0900

    Update free_talking.py

--- pages/free_talking.py
+++ pages/free_talking.py
@@ -81,7 +81,7 @@ def display_messages():
 # Streamlit UI
 
 # 메인 화면 구성
-st.header("✨인공지능 영어 선생님: 잉글링👱🏾‍♂️")
+st.header("✨인공지능 영어 선생님 잉글링👱🏾‍♂️")
 st.subheader("😃자유롭게 대화하기")
 st.divider()
 
@@ -100,8 +100,9 @@ with st.expander("❗❗ 글상자를 펼쳐 사용방법을 읽어보세요 �
     """
     ,  unsafe_allow_html=True)
     st.divider()
-    st.write("🔹이번 단원과 관련하여 궁금한 점이나 영어에 대해 전반적으로 궁금한 점을 한국어나 영어로 질문하세요.")
-    st.write("🔹영어로 인공지능 선생님과 자유롭게 대화할 수도 있어요.")
+    st.write("🔸이번 단원과 관련하여 궁금한 점을 물어보세요." 
+    st.write("🔸영어에 대해 전반적으로 궁금한 점을 한국어나 영어로 질문하세요.")
+    st.write("🔸영어로 잉글링과 자유롭게 대화할 수도 있어요.")
 
     
 # 버튼 배치
